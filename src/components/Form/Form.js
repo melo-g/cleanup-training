e0af0f8b67a222148c3f1052694f90eb4a7a1189
aaroncarmelo.galia <aaron.carmelo.galia@gmail.com>
2019-11-24 20:43:54 +0800

    fixed: some responsiveness and format

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import '../../App.css'
 import border from '../../images/border.png'
 import formData from './data.js'
 import Style from '../Style/Style.js'
 
 export default class Form extends Component {
-	render () {
-	//console.log("hello", formData)
+	render() {
+		//console.log("hello", formData)
 		return (
 			<div className="form">
-				
+
 				<p className="form-title">キッチン収納診断</p>
 				<p className="form-desc">当てはまるものを「はい」か「いいえ」で選択ください。</p>
 
@@ -17,36 +17,37 @@ export default class Form extends Component {
 					<div class="form-body">
 						<img src={border} alt="border" />
 
-						{formData.map( data =>
-							
-						<div className="form-body-content" key={data.id}>
-							<div className="item">
-								<p className="item-question">Q</p>
-								<p className="item-number">{data.number}.</p>
+						{formData.map(data =>
+
+							<div className="form-body-content" key={data.id}>
+								<div className="item">
+									<p className="item-question">Q</p>
+									<p className="item-number">{data.number}</p>
+								</div>
+
+								<div className="question">
+									<p>{data.question}</p>
+								</div>
+
+								<div className="answer">
+									<button className="answer-yes">はい</button>
+									<button className="answer-no">いいえ</button>
+								</div>
 							</div>
 
-							<div className="question">
-								<p>{data.question}</p>
-							</div>
-
-							<div className="answer">
-								<button className="answer-yes">はい</button>
-								<button className="answer-no">いいえ</button>
-							</div>
-						</div>
-							)}
+						)}
 
 						<p className="email">メールアドレスをお教えください。<span>（必須）</span></p>
-						
+
 						<p className="website">abc@oikura.cleanup.jp</p>
 
 						<p className="policy"><a href="#">個人情報保護方針のご確認*</a></p>
-						</div>
+					</div>
 
 				</div>
-					<button className="diagnosis">診断結果を見る</button>
-					<Style/>
-			</div>			
-			);
+				<button className="diagnosis">診断結果を見る</button>
+				<Style />
+			</div>
+		);
 	}
 }
